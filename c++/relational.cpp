4280ba392aa46dfea617c5f0b46e2c0606817f24
Meriprizerihun <meriprizerihun@gmail.com>
2026-04-21 11:41:59 +0300

    relational

---
+++ c++/relational.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int A=30, B=50;
+	bool grater =A>B;
+	bool leasthan =A<B;
+	bool leasthanorequal=A<=B;
+	bool graterthanorequal=A>=B;
+	bool equal= A=B;
+	bool notequal=A!=B;
+	cout<<"A:         " <<"B:   "<<endl;
+	cout <<"grater : "<<grater<< endl;
+cout <<"grater : "<<grater<< endl;
+cout <<"leasthan : "<<leasthan<< endl;
+cout <<"leasthanorequal : "<<leasthanorequal<< endl;
+cout <<"graterthanorequal : "<<graterthanorequal<< endl;
+cout <<"equal : "<<equal<< endl;
+cout <<"notequal : "<<notequal<< endl;	
+	
+	return 0;
+}
