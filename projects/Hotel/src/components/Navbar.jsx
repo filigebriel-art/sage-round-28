42a934796403aa935c7e6d665181872f5367bb61
filmon <filigebriel@gmail.com>
2026-05-30 23:11:20 +0300

    real project

---
+++ projects/Hotel/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Navbar(){
+    return(
+    <nav  className="navbar">
+        <h2>Hotel</h2>
+        <ul>
+            <li>Home</li>
+            <li>Rooms</li>
+            <li>Login</li>
+            <li>About</li>
+        </ul>
+    </nav>
+    )
+}
