1681bbc013d1c0972e8aed83daf94c9a48bf5883
filigebriel-art <filigebriel@gmail.com>
2026-06-05 02:37:49 -0700

    calculator on react

---
+++ frontend/react project/calculator/src/App.jsx
@@ -0,0 +1,8 @@
+import Calculator from "./Calculator";
+
+export default function App(){
+  return<>
+  
+  <Calculator/>
+  </>
+}
---
+++ frontend/react project/calculator/src/Calculator.jsx
@@ -0,0 +1,59 @@
+import { evaluate } from "mathjs"
+import { useState } from "react"
+
+
+function Calculator(){
+
+    const[input,setInput]=useState("")
+
+    const addValue=(value)=>{
+        setInput(input+value)
+    }
+    const calculate=()=>{
+        try {
+            setInput(evaluate(input).toString())
+        }
+        catch{
+            setInput("Error")
+        }
+    }
+    const clearInput=()=>{
+        setInput("")
+    }
+
+
+    
+    return(
+    <div>
+    
+<input type="text"
+value={input}
+readOnly
+ />
+ <br /><br />
+
+    <button onClick={()=>addValue("1")}>1</button>
+    <button onClick={()=>addValue("2")}>2</button>
+    <button onClick={()=>addValue("3")}>3</button>
+    <button onClick={()=>addValue("+")}>+</button>
+     <br /><br />    
+     <button onClick={()=>addValue("4")}>4</button>
+     <button onClick={()=>addValue("5")}>5</button>
+     <button onClick={()=>addValue("6")}>6</button>
+     <button onClick={()=>addValue("-")}>-</button>
+     <br /><br />
+
+     <button onClick={()=>addValue("7")}>7</button>
+     <button onClick={()=>addValue("8")}>8</button>
+     <button onClick={()=>addValue("9")}>9</button>
+     <button onClick={()=>addValue("*")}>*</button>
+    
+    <br /><br />
+    <button onClick={()=>addValue("0")}>0</button>
+    <button onClick={()=>addValue("/")}>/</button>
+    <button onClick={calculate}>=</button>
+    <button onClick={clearInput}>c</button>
+    </div>
+
+)}
+export default Calculator
